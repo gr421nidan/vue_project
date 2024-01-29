0e1c98ecde1048ba7351ad051421c562cc7dd1f8
gr421_nidan <nikushkina.dasha@yandex.ru>
2024-01-29 13:41:33 +0700

    Работа с привязкой атрибутов, вывод изображения и добавление текста в атрибут alt. Подключение стилей

--- js/main.js
+++ js/main.js
@@ -2,7 +2,9 @@ let app = new Vue({
     el: '#app',
     data: {
         product: "Socks",
-        description: "A pair of warm, fuzzy socks."
-    }
+        image: "./assets/vmSocks-blue-onWhite.jpg",
+        altText: "A pair of socks"
 
+    }
 })
+
