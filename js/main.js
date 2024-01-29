bf84aa36e1ce04ff6def7a8cace72982cbae7dea
gr421_nidan <nikushkina.dasha@yandex.ru>
2024-01-29 14:26:53 +0700

    Выполнение практической №4

--- js/main.js
+++ js/main.js
@@ -15,7 +15,9 @@ let app = new Vue({
                 variantId: 2235,
                 variantColor: 'blue'
             }
-        ]
+        ],
+        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL']
+
 
     }
 })
