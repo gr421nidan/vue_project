275d18210e5fa3474605cb679c49b53e0eca8d46
gr421nidan <nikushkina.dasha@yandex.ru>
2024-01-29 20:40:41 +0700

    Добавление image и inStock в виде вычисляемых свойств

--- js/main.js
+++ js/main.js
@@ -3,20 +3,23 @@ let app = new Vue({
     data: {
         product: "Socks",
         brand: 'Vue Mastery',
-        image: "./assets/vmSocks-blue-onWhite.jpg",
+        selectedVariant: 0,
         altText: "A pair of socks",
         inStock: true,
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+
         variants: [
             {
                 variantId: 2234,
                 variantColor: 'green',
                 variantImage: "./assets/vmSocks-green-onWhite.jpg",
+                variantQuantity: 10
             },
             {
                 variantId: 2235,
                 variantColor: 'blue',
                 variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+                variantQuantity: 0
             }
         ],
         cart: 0,
@@ -26,15 +29,25 @@ let app = new Vue({
         addToCart() {
             this.cart += 1
         },
-        updateProduct(variantImage) {
-            this.image = variantImage
-        },
+        updateProduct(index) {
+            this.selectedVariant = index;
+            console.log(index);
+        }
+
 
     },
     computed: {
         title() {
             return this.brand + ' ' + this.product;
-        }
-    }
+        },
+        image() {
+            return this.variants[this.selectedVariant].variantImage;
+        },
+        inStock(){
+            return this.variants[this.selectedVariant].variantQuantity
+        },
+
+    },
+
 })
 
